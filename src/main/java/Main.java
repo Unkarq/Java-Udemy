b90148f34e9280414a06bb5e1806f58ebfdff04d
Unkarq <unkar91@gmail.com>
2019-04-14 13:57:45 +0200

    001  -  if

---
+++ src/main/java/Main.java
@@ -0,0 +1,28 @@
+public class Main {
+
+    public static void main(String[] args) {
+
+        int a = 5,
+                b = 7,
+                c = 9;
+
+//        System.out.println(a!=b);
+//
+//        if(a !=b && c > 5)
+//            System.out.println("test");
+//
+//
+
+        if (a > b)
+            System.out.println("a > b");
+        else if (a < b) {
+            System.out.println("a < b");
+            System.out.println("It's working");
+        } else
+            System.out.println("a == b");
+
+
+    }
+
+
+}
